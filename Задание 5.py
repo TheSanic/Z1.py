6886594eec9d719d1be1ee91a5b4365bad84b37e
TheSanic <Forestyt@yandex.ru>
2017-05-18 02:19:59 +0700

    Delete Задание 5.py

--- Задание 5.py
+++
@@ -1,9 +0,0 @@
-f = open('output.txt','w')
-x = ('z')
-y = ('a')
-for o in x:
-    for i in y:
-        if(((o == 'z') + (i == 'a') == 25) and ((o == -'z') + (i == -'a') == 1/6)):
-            print(o, file = f)
-            print(i, file = f)
-f.close()
